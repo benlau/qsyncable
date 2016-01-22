68a11d95105183a0d5017f68604d1bdf382c3f55
Ben Lau <xbenlau@gmail.com>
2016-01-22 18:20:52 +0800

    Add "backtrace" on crash

--- tests/qsyncableunittests/main.cpp
+++ tests/qsyncableunittests/main.cpp
@@ -1,10 +1,29 @@
 #include <QString>
 #include <QtTest>
+#include <execinfo.h>
+#include <signal.h>
+#include <unistd.h>
 #include "testrunner.h"
 #include "qsyncabletests.h"
 #include "benchmarktests.h"
 
+
+void handleBacktrace(int sig) {
+  void *array[100];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 100);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
+
 int main(int argc, char* argv[]) {
+    signal(SIGSEGV, handleBacktrace);
 
     QGuiApplication app(argc, argv);
     TestRunner runner;
