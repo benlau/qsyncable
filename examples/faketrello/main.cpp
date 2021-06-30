c75068fcd3d8867088d8455c70882bd1cdb015d8
Ernesto Cruz <ernesto@ecruzolivera.tech>
2021-06-30 00:31:30 -0500

    public register qml types function

--- examples/faketrello/main.cpp
+++ examples/faketrello/main.cpp
@@ -1,12 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include "appdelegate.h"
+#include "QSListModel"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
     Q_UNUSED(app);
-
+    registerQSyncableTypes();
     AppDelegate delegate;
 
     return delegate.run();
